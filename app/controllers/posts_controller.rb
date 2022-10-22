36c8e62eba5736aad3976dbb83bdcd9404cc48e1
carlos espinal <andrescar666@gmail.com>
2022-10-22 15:37:55 -0500

    Controller post new create y view new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,24 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save!
+      redirect_to root_path, notice: "Post guardado exitosamente"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -14,9 +27,12 @@ class PostsController < ApplicationController
   def update
   end
 
-  def show
+  def destroy
   end
 
-  def destroy
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :type_activity, :description, :date, :grade_id, :subject_id)
   end
 end
