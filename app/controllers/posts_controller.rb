2e77ac0696ae43f45c1a69c8af07c42b1fd4f6d0
carlos espinal <andrescar666@gmail.com>
2022-10-22 16:49:33 -0500

    Controller y View de: edit, update y destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,12 +22,22 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path, notice: "Actualización exitosa"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to post_path, notice: "Eliminación exitosa", status: :see_other
   end
 
   private
