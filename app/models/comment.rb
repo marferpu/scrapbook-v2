678e82cf6c031fa374f2ebbea5dc551d02674945
daniela-lotero <daniela.loterom@gmail.com>
2022-10-23 16:57:18 -0500

    comments funciona el destroy, pero no el new, falta solucionar bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :content, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :classrooms, only: %i[create]
 
   resources :posts do
-    resources :comments, only: %i[create]
+    resources :comments, only: %i[new]
   end
   resources :comments, only: :destroy
 end
