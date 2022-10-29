4cb1082ba8b19f7c3c3f44c3ff5fcce78bccc865
carlos espinal <andrescar666@gmail.com>
2022-10-29 09:41:26 -0500

    Cambio formato fecha al show form

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ScrapbookV2
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    I18n.available_locales = [:en, :es]
     config.i18n.default_locale = 'es-CO'
 
     # Configuration for the application, engines, and railties goes here.
